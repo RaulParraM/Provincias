71b20801be99bc8b992668e1c073af906491580e
RaulParraM <raosprat@hotmail.com>
2019-02-20 08:19:10 +0100

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Provncias
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CBProvincias = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // CBProvincias
+            // 
+            this.CBProvincias.FormattingEnabled = true;
+            this.CBProvincias.Location = new System.Drawing.Point(222, 12);
+            this.CBProvincias.Name = "CBProvincias";
+            this.CBProvincias.Size = new System.Drawing.Size(273, 24);
+            this.CBProvincias.TabIndex = 0;
+            this.CBProvincias.Text = " Provincias";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CBProvincias);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox CBProvincias;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Provncias
+{
+    public partial class Form1 : Form
+    {
+        private XmlDocument doc;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\Provincias.xml";
+            doc = new XmlDocument();
+            doc.Load(path);
+            XmlNodeList ProvinciasList = doc.SelectNodes("Provincias/Provincia");
+            XmlNode provincia;
+            for(int i = 0; i < ProvinciasList.Count; ++i)
+            {
+                provincia = ProvinciasList.Item(i);
+                string provinciaLeida = provincia.SelectSingleNode("Nombre").InnerText;
+                CBProvincias.Items.Add(provinciaLeida);
+            }
+        }
+    }
+}
